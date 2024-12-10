3623b72e2cd75779185ba9b7a9655878e101ba46
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2024-12-10 08:51:14 +0300

    Update the product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,5 @@
-export default async function Product({ params }: { params: { id: string } }) {
-    const { id } = params; // No need to await
+export default function Product({ params }: { params: { id: string } }) {
+    const { id } = params; // params is a plain object, no need to await
     return <h1>Product: {id}</h1>;
   }
   
